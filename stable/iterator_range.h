9ec0ac64cc00b673651ea7207ce8992ac31cea1e
OleErikP@gmail.com <OleErikPeistorpet@users.noreply.github.com>
2014-11-03 02:08:03 +0000

    Renamed basic_range_util.h

--- stable/iterator_range.h
+++ stable/iterator_range.h
@@ -4,8 +4,9 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 
+#include "basic_util.h"
+
 #include <boost/range/iterator_range_core.hpp>
-#include <iterator>
 
 
 namespace oetl
--- stable/util.h
+++ stable/util.h
@@ -6,7 +6,7 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 
-#include "basic_range_util.h"
+#include "basic_util.h"
 
 #include <algorithm>
 #include <cstring>
