08ad20ae3fe09514f11064a15b30d70a09984347
OleErikP@gmail.com <OleErikPeistorpet@users.noreply.github.com>
2014-11-20 19:48:52 +0000

    Fixed compilation errors. fixcap_array unit testing, copy-pasted for now.

--- unit_test/forward_decl_test.h
+++ unit_test/forward_decl_test.h
@@ -1,4 +1,22 @@
 #include "dynarray.h"
+#include "iterator_range.h"
+#include "gtest/gtest.h"
+#include <deque>
+
+
+struct Deleter
+{
+	static int callCount;
+
+	void operator()(double * p)
+	{
+		++callCount;
+		delete p;
+	}
+};
+
+typedef std::unique_ptr<double, Deleter> DoublePtr;
+
 
 class ForwDeclared;
 
