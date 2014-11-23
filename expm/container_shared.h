6fa893908b65b065279dc06884cb4743ec54782f
OleErikP@gmail.com <OleErikP@gmail.com@1454966e-2555-dca1-59cb-b507c9ac989f>
2014-11-23 12:18:28 +0000

    Tree changes

--- expm/container_shared.h
+++ expm/container_shared.h
@@ -6,7 +6,7 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 
-#include "basic_util.h"
+#include "core_util.h"
 
 #ifndef OETL_NO_BOOST
 #include <boost/align/aligned_alloc.hpp>
