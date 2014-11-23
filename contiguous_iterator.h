44be211770d0b3b1d3a475edd595c874c428281b
OleErikP@gmail.com <OleErikPeistorpet@users.noreply.github.com>
2014-11-23 02:46:53 +0000

    Renaming and minor experimentation

--- contiguous_iterator.h
+++ contiguous_iterator.h
@@ -12,13 +12,11 @@
 namespace oetl
 {
 
-template<typename, typename> class array_debug_iterator;
-
 /** @brief Iterator wrapping for pointer with error checking
 *
 * The class has significant overhead, is meant for debug builds only  */
 template<typename ConstQualValT, typename Container>
-class array_debug_iterator
+class cntigus_ctr_dbg_iterator
 {
 #define OETL_ARRITER_CHECK_DEREFABLE  \
 	MEM_BOUND_ASSERT( static_cast<typename Container::size_type>(_pElem - _myCont->data()) < _myCont->size() )
@@ -39,17 +37,17 @@ public:
 	using reference  = ConstQualValT &;
 	using difference_type = std::ptrdiff_t;
 
-	array_debug_iterator() : _myCont(nullptr) {
+	cntigus_ctr_dbg_iterator() : _myCont(nullptr) {
 	}
 
 	// construct with position in data and pointer to container
-	array_debug_iterator(pointer pos, const Container * container)
+	cntigus_ctr_dbg_iterator(pointer pos, const Container * container)
 	 :	_pElem(pos), _myCont(container) {
 	}
 
-	operator array_debug_iterator<value_type const, Container>() const
-	{
-		return {_pElem, _myCont};
+	template< typename ValT2, typename = std::enable_if_t<std::is_same<value_type, ValT2>::value> >
+	cntigus_ctr_dbg_iterator(const cntigus_ctr_dbg_iterator<ValT2, Container> & other)
+	 :	_pElem(other._pElem), _myCont(other._myCont) {
 	}
 
 	reference operator*() const
@@ -64,7 +62,7 @@ public:
 		return _pElem;
 	}
 
-	array_debug_iterator & operator++()
+	cntigus_ctr_dbg_iterator & operator++()
 	{	// preincrement
 #	if OETL_MEM_BOUND_DEBUG_LVL >= 3
 		MEM_BOUND_ASSERT( _pElem < to_ptr(_myCont->end()) );
@@ -73,14 +71,14 @@ public:
 		return *this;
 	}
 
-	array_debug_iterator operator++(int)
+	cntigus_ctr_dbg_iterator operator++(int)
 	{	// postincrement
 		auto tmp = *this;
 		++(*this);
 		return tmp;
 	}
 
-	array_debug_iterator & operator--()
+	cntigus_ctr_dbg_iterator & operator--()
 	{	// predecrement
 #	if OETL_MEM_BOUND_DEBUG_LVL >= 3
 		MEM_BOUND_ASSERT(_myCont->data() < _pElem);
@@ -89,14 +87,14 @@ public:
 		return *this;
 	}
 
-	array_debug_iterator operator--(int)
+	cntigus_ctr_dbg_iterator operator--(int)
 	{	// postdecrement
 		auto tmp = *this;
 		--(*this);
 		return tmp;
 	}
 
-	array_debug_iterator & operator+=(difference_type offset)
+	cntigus_ctr_dbg_iterator & operator+=(difference_type offset)
 	{	// add integer to pointer
 #	if OETL_MEM_BOUND_DEBUG_LVL >= 3
 		MEM_BOUND_ASSERT( offset >= _myCont->data() - _pElem
@@ -106,7 +104,7 @@ public:
 		return *this;
 	}
 
-	array_debug_iterator & operator-=(difference_type offset)
+	cntigus_ctr_dbg_iterator & operator-=(difference_type offset)
 	{	// subtract integer from pointer
 #	if OETL_MEM_BOUND_DEBUG_LVL >= 3
 		MEM_BOUND_ASSERT( offset <= _pElem - _myCont->data()
@@ -116,20 +114,20 @@ public:
 		return *this;
 	}
 
-	array_debug_iterator operator +(difference_type offset) const
+	cntigus_ctr_dbg_iterator operator +(difference_type offset) const
 	{	// return this + integer
 		auto tmp = *this;
 		return tmp += offset;
 	}
 
-	array_debug_iterator operator -(difference_type offset) const
+	cntigus_ctr_dbg_iterator operator -(difference_type offset) const
 	{	// return this - integer
 		auto tmp = *this;
 		return tmp -= offset;
 	}
 
 	template<typename ValT2>
-	difference_type operator -(const array_debug_iterator<ValT2, Container> & right) const
+	difference_type operator -(const cntigus_ctr_dbg_iterator<ValT2, Container> & right) const
 	{	// return difference of iterators
 		OETL_ARRITER_CHECK_COMPAT(right);
 		return _pElem - right._pElem;
@@ -141,50 +139,44 @@ public:
 	}
 
 	template<typename ValT2>
-	bool operator==(const array_debug_iterator<ValT2, Container> & right) const
+	bool operator==(const cntigus_ctr_dbg_iterator<ValT2, Container> & right) const
 	{
-#	if OETL_MEM_BOUND_DEBUG_LVL >= 3
-		MEM_BOUND_ASSERT(_myCont || right._myCont);
-#	endif
 		return _pElem == right._pElem;
 	}
 
 	template<typename ValT2>
-	bool operator!=(const array_debug_iterator<ValT2, Container> & right) const
+	bool operator!=(const cntigus_ctr_dbg_iterator<ValT2, Container> & right) const
 	{
-#	if OETL_MEM_BOUND_DEBUG_LVL >= 3
-		MEM_BOUND_ASSERT(_myCont || right._myCont);
-#	endif
 		return _pElem != right._pElem;
 	}
 
 	template<typename ValT2>
-	bool operator <(const array_debug_iterator<ValT2, Container> & right) const
+	bool operator <(const cntigus_ctr_dbg_iterator<ValT2, Container> & right) const
 	{
 		OETL_ARRITER_CHECK_COMPAT(right);
 		return _pElem < right._pElem;
 	}
 
 	template<typename ValT2>
-	bool operator >(const array_debug_iterator<ValT2, Container> & right) const
+	bool operator >(const cntigus_ctr_dbg_iterator<ValT2, Container> & right) const
 	{
 		OETL_ARRITER_CHECK_COMPAT(right);
 		return _pElem > right._pElem;
 	}
 
 	template<typename ValT2>
-	bool operator<=(const array_debug_iterator<ValT2, Container> & right) const
+	bool operator<=(const cntigus_ctr_dbg_iterator<ValT2, Container> & right) const
 	{
 		return !(*this > right);
 	}
 
 	template<typename ValT2>
-	bool operator>=(const array_debug_iterator<ValT2, Container> & right) const
+	bool operator>=(const cntigus_ctr_dbg_iterator<ValT2, Container> & right) const
 	{
 		return !(*this < right);
 	}
 
-	friend pointer to_ptr(array_debug_iterator it) NOEXCEPT
+	friend pointer to_ptr(cntigus_ctr_dbg_iterator it) NOEXCEPT
 	{	// return pointer (unchecked)
 		return it._pElem;
 	}
@@ -193,32 +185,29 @@ protected:
 	pointer           _pElem;  // wrapped pointer
 	const Container * _myCont;
 
-	friend class array_debug_iterator;
+	template<typename, typename>
+	friend class cntigus_ctr_dbg_iterator;
 
 #undef OETL_ARRITER_CHECK_COMPAT
 #undef OETL_ARRITER_CHECK_DEREFABLE
 };
 
 template<typename T, typename C> inline
-array_debug_iterator<T, C> operator +(typename array_debug_iterator<T, C>::difference_type offset,
-									  array_debug_iterator<T, C> iter)
+cntigus_ctr_dbg_iterator<T, C> operator +(typename cntigus_ctr_dbg_iterator<T, C>::difference_type offset,
+										  cntigus_ctr_dbg_iterator<T, C> iter)
 {	// add offset to iterator
 	return iter += offset;
 }
 
 #if OETL_MEM_BOUND_DEBUG_LVL >= 2
 
-template<typename Container>
-using array_iterator       = array_debug_iterator<typename Container::value_type, Container>;
-template<typename Container>
-using array_const_iterator = array_debug_iterator< std::add_const_t<typename Container::value_type>, Container >;
+template<typename ConstQualValT, typename Container>
+using contiguous_iterator = cntigus_ctr_dbg_iterator<ConstQualValT, Container>;
 
 #else
 
-template<typename Container>
-using array_iterator       = typename Container::value_type *;
-template<typename Container>
-using array_const_iterator = const typename Container::value_type *;
+template<typename ConstQualValT, typename>
+using contiguous_iterator = ConstQualValT *;
 
 #endif
 
