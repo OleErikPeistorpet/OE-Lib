313e4b2fd70229c90854b59312d361a39ac92dc0
Ole Erik Peistorpet <oleerikp@gmail.com>
2015-03-16 19:51:33 +0100

    Copyright year
    added 2015

--- dynarray.h
+++ dynarray.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// Copyright © 2014 Ole Erik Peistorpet
+// Copyright 2014, 2015 Ole Erik Peistorpet
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
