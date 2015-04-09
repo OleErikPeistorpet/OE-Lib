661a4fb2a2e551a39d01020c826917c321c5b621
OleErikPeistorpet <oleerikp@gmail.com>
2015-04-09 11:38:43 +0200

    trivially relocatable Eigen Quaternion
    and Transform, etc.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A very fast substitute for C++ std::vector (and std::copy)
 For better display of dynarray and the iterators in the Visual Studio debugger, copy `oe_lib.natvis` to:
 `<My Documents>\Visual Studio 2013\Visualizers` (should also work for newer Visual Studio, just different year in path)
 
-To use dense matrixes from the Eigen library in dynarray: include `compat/eigen_dense.h`
+To use dense matrixes, quaternions, etc. from the Eigen library in dynarray: include `compat/eigen_dense.h`
--- compat/eigen_dense.h
+++ compat/eigen_dense.h
@@ -11,7 +11,22 @@
 
 namespace oel
 {
+
 template<typename S, int R, int C, int O, int MR, int MC>
 struct is_trivially_relocatable< Eigen::Matrix<S, R, C, O, MR, MC> >
  :	std::true_type {};
+
+template<typename S, int O>
+struct is_trivially_relocatable< Eigen::Quaternion<S, O> > : std::true_type {};
+
+template<typename S, int D, int M, int O>
+struct is_trivially_relocatable< Eigen::Transform<S, D, M, O> >
+ :	std::true_type {};
+
+template<typename S>
+struct is_trivially_relocatable< Eigen::AngleAxis<S> > : std::true_type {};
+
+template<typename S>
+struct is_trivially_relocatable< Eigen::Rotation2D<S> > : std::true_type {};
+
 }
