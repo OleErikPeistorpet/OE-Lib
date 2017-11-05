f0b958a4152c8399524cf99ad2b58cee69e94dc1
Ole Erik Peistorpet <oleerikp@gmail.com>
2017-11-05 16:09:35 +0100

    Moved OEL_ALWAYS_ASSERT, OEL_HALT and other error related stuff to new file

---
+++ error_handling.h
@@ -0,0 +1,84 @@
+#pragma once
+
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include "auxi/macros.h"
+
+#include <stdexcept>
+
+
+/** @file
+*/
+
+#if !defined(NDEBUG) && !defined(OEL_MEM_BOUND_DEBUG_LVL)
+/** @brief Undefined/0: no array index and iterator checks. 1: most debug checks. 2: all checks, often slow.
+*
+* 0 (or undefined by NDEBUG defined) is not binary compatible with levels 1 and 2. */
+	#define OEL_MEM_BOUND_DEBUG_LVL 2
+#endif
+
+#ifndef OEL_ALWAYS_ASSERT
+	#ifndef OEL_HALT
+	/** Could throw an exception instead, or do whatever. If it contains assembly, compilation will likely fail.
+	*
+	* Example: @code
+	#define OEL_HALT(failedCond) throw std::logic_error(failedCond ", assertion failed in " __FILE__)
+	@endcode  */
+		#if _MSC_VER
+		#define OEL_HALT(failedCond) __debugbreak()
+		#elif OEL_HAS_BUILTIN_TRAP
+		#define OEL_HALT(failedCond) __builtin_trap()
+		#else
+		#define OEL_HALT(failedCond) std::abort()
+		#endif
+	#endif
+
+	//! Executes OEL_HALT on failure. Can be defined to standard assert or your own
+	#define OEL_ALWAYS_ASSERT(expr)  \
+		OEL_CONST_COND  \
+		do {  \
+			if (!(expr)) OEL_HALT(#expr);  \
+		} while (false)
+#endif
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+#undef OEL_HAS_BUILTIN_TRAP
+
+#if OEL_MEM_BOUND_DEBUG_LVL
+	#define OEL_ASSERT_MEM_BOUND  OEL_ALWAYS_ASSERT
+#else
+	#define OEL_ASSERT_MEM_BOUND(expr) ((void) 0)
+#endif
+#if !defined(NDEBUG)
+	#define OEL_ASSERT  OEL_ALWAYS_ASSERT
+#else
+	#define OEL_ASSERT(expr) ((void) 0)
+#endif
+
+
+namespace oel
+{
+namespace _detail
+{
+	struct Throw
+	{	// at namespace scope this produces warnings of unreferenced function or failed inlining
+	#if _MSC_VER
+		__declspec(noreturn)
+	#else
+		__attribute__((noreturn))
+	#endif
+		static void OutOfRange(const char * what)
+		{
+			OEL_THROW(std::out_of_range(what));
+		}
+	};
+}
+
+}
--- util.h
+++ util.h
@@ -7,7 +7,7 @@
 
 
 #include "user_traits.h"
-#include "auxi/macros.h"
+#include "error_handling.h"
 
 #ifndef OEL_NO_BOOST
 	#include <boost/iterator/iterator_categories.hpp>
@@ -23,45 +23,11 @@
 * Contains as_signed/as_unsigned, index_valid, ssize, adl_begin, adl_end, deref_args and more.
 */
 
-#if !defined(NDEBUG) && !defined(OEL_MEM_BOUND_DEBUG_LVL)
-/** @brief Undefined/0: no array index and iterator checks. 1: most debug checks. 2: all checks, often slow.
-*
-* 0 (or undefined by NDEBUG defined) is not binary compatible with levels 1 and 2. */
-	#define OEL_MEM_BOUND_DEBUG_LVL 2
-#endif
-
 #if defined(NDEBUG) && OEL_MEM_BOUND_DEBUG_LVL == 0
 	#define OEL_NOEXCEPT_NDEBUG noexcept
 #else
 	#define OEL_NOEXCEPT_NDEBUG
 #endif
-
-
-#ifndef OEL_ALWAYS_ASSERT
-	#ifndef OEL_HALT
-	/** Could throw an exception instead, or do whatever. If it contains assembly, compilation will likely fail.
-	*
-	* Example: @code
-	#define OEL_HALT(failedCond) throw std::logic_error(failedCond ", assertion failed in " __FILE__)
-	@endcode  */
-		#if _MSC_VER
-		#define OEL_HALT(failedCond) __debugbreak()
-		#elif OEL_HAS_BUILTIN_TRAP
-		#define OEL_HALT(failedCond) __builtin_trap()
-		#else
-		#define OEL_HALT(failedCond) std::abort()
-		#endif
-	#endif
-
-	//! Executes OEL_HALT on failure. Can be defined to standard assert or your own
-	#define OEL_ALWAYS_ASSERT(expr)  \
-		OEL_CONST_COND  \
-		do {  \
-			if (!(expr)) OEL_HALT(#expr);  \
-		} while (false)
-#endif
-
-#undef OEL_HAS_BUILTIN_TRAP
 
 
 namespace oel
@@ -104,9 +70,9 @@ constexpr bool index_valid(const SizedRange & r, std::int32_t index)  { return 0
 
 template<typename SizedRange> inline
 constexpr bool index_valid(const SizedRange & r, std::int64_t index)
-{	// assumes that r.size() is never greater than INT64_MAX
-	return static_cast<std::uint64_t>(index) < as_unsigned(oel::ssize(r));
-}
+	{	// assumes that r.size() is never greater than INT64_MAX
+		return static_cast<std::uint64_t>(index) < as_unsigned(oel::ssize(r));
+	}
 ///@}
 
 
@@ -209,19 +175,6 @@ const default_init; //!< An instance of default_init_tag to pass
 // The rest of the file is not for users (implementation)
 
 
-#if OEL_MEM_BOUND_DEBUG_LVL
-	#define OEL_ASSERT_MEM_BOUND  OEL_ALWAYS_ASSERT
-#else
-	#define OEL_ASSERT_MEM_BOUND(expr) ((void) 0)
-#endif
-#if !defined(NDEBUG)
-	#define OEL_ASSERT  OEL_ALWAYS_ASSERT
-#else
-	#define OEL_ASSERT(expr) ((void) 0)
-#endif
-
-////////////////////////////////////////////////////////////////////////////////
-
 using std::size_t;
 
 
@@ -282,21 +235,6 @@ using iterator_traversal_t
 
 namespace _detail
 {
-	struct Throw
-	{	// at namespace scope this produces warnings of unreferenced function or failed inlining
-	#if _MSC_VER
-		__declspec(noreturn)
-	#else
-		__attribute__((noreturn))
-	#endif
-		static void OutOfRange(const char * what)
-		{
-			OEL_THROW(std::out_of_range(what));
-		}
-	};
-
-////////////////////////////////////////////////////////////////////////////////
-
 	inline void MemcpyMaybeNull(void * dest, const void * src, size_t nBytes)
 	{	// memcpy(nullptr, nullptr, 0) is undefined behavior.
 		// The problem is that checking can have significant performance hit
