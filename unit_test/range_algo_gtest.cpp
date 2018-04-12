3dd48376c5903de98fbeb189ffda5ea5e4c50230
Ole Erik Peistorpet <oleerikp@gmail.com>
2018-04-12 13:32:25 +0200

    Restored a unit test mistakenly disabled for MSVC

--- unit_test/range_algo_gtest.cpp
+++ unit_test/range_algo_gtest.cpp
@@ -73,7 +73,7 @@ TEST(rangeTest, countedView)
 	EXPECT_EQ(2, test.end()[-1]);
 }
 
-#if !defined OEL_NO_BOOST and OEL_GCC_VERSION >= 408
+#if !defined OEL_NO_BOOST
 TEST(rangeTest, viewTransform)
 {
 	using namespace oel;
