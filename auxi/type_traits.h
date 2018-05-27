cf7324ca3d6c5ca00f738cbced15c16a99cb81de
Ole Erik Peistorpet <oleerikp@gmail.com>
2018-05-27 20:54:32 +0200

    Removed all_true (didn't seem useful)

--- auxi/type_traits.h
+++ auxi/type_traits.h
@@ -25,10 +25,6 @@ using enable_if = typename std::enable_if<Condition, int>::type;
 
 template<bool...> struct bool_pack_t;
 
-//! If all of Vs are equal to true, this is-a true_type, else false_type
-template<bool... Vs>
-using all_true   = std::is_same< bool_pack_t<true, Vs...>, bool_pack_t<Vs..., true> >;
-
 /** @brief Similar to std::conjunction, but is not short-circuiting
 *
 * Example: @code
@@ -37,8 +33,8 @@ void ProcessNumbers(Ts... n) {
 	static_assert(oel::all_< std::is_arithmetic<Ts>... >::value, "Only arithmetic types, please");
 @endcode  */
 template<typename... BoolConstants>
-struct all_ : all_true<BoolConstants::value...> {};
-
+struct all_   : std::is_same< bool_pack_t<true, BoolConstants::value...>,
+                              bool_pack_t<BoolConstants::value..., true> > {};
 
 
 //! If an IteratorSource range can be copied to an IteratorDest range with memmove, is-a true_type, else false_type
@@ -48,7 +44,7 @@ struct can_memmove_with;
 
 //! Range::difference_type if present, else std::ptrdiff_t
 template<typename Range>
-using range_difference_t  = decltype( _detail::DiffT<Range>(0) );
+using range_difference_t   = decltype( _detail::DiffT<Range>(0) );
 
 template<typename Iterator>
 using iterator_difference_t = typename std::iterator_traits<Iterator>::difference_type;
@@ -76,7 +72,7 @@ using iterator_is_random_access = std::is_base_of< random_access_traversal_tag, 
 
 //! Part of std::allocator_traits for C++17
 template<typename T>
-using is_always_equal  = decltype( _detail::IsAlwaysEqual<T>(0) );
+using is_always_equal   = decltype( _detail::IsAlwaysEqual<T>(0) );
 
 
 #if defined __GLIBCXX__ and __GNUC__ == 4
