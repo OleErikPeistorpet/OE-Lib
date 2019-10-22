4e054a7f129552b0ac6f4000e8f93e8ebdad7a15
Ole Erik Peistorpet <oleerikp@gmail.com>
2019-10-23 01:07:51 +0200

    Added -fstrict-aliasing for tests, and MS lib warning fix
    This fix fixes a previous fix that didn't fix the C4996 warning

--- unit_test/dynarray_other_gtest.cpp
+++ unit_test/dynarray_other_gtest.cpp
@@ -1,7 +1,7 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#ifdef _CPPLIB_VER
+#ifdef _MSC_VER
 #define _SILENCE_CXX17_OLD_ALLOCATOR_MEMBERS_DEPRECATION_WARNING
 #endif
 
