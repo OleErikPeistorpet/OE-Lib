02605267cac2f23b9f1aa1a3442c4861acd521ce
Ole Erik Peistorpet <oleerikp@gmail.com>
2020-05-01 15:04:46 +0200

    transform_iterator supports move-only underlying iterator

--- auxi/transform_iterator.h
+++ auxi/transform_iterator.h
@@ -20,7 +20,7 @@ namespace _detail
 		I inner;
 		F _fun;
 
-		OEL_ALWAYS_INLINE const F & Func() const noexcept { return _fun; }
+		OEL_ALWAYS_INLINE const F & Func() const { return _fun; }
 	};
 
 	template< typename Iterator_MSVC_needs_unique_name, typename Empty_function_object_MSVC_name >
@@ -30,71 +30,101 @@ namespace _detail
 		Iterator_MSVC_needs_unique_name inner;
 
 		TightPair(Iterator_MSVC_needs_unique_name it, Empty_function_object_MSVC_name f)
-		 :	Empty_function_object_MSVC_name(f), inner(it) {
+		 :	Empty_function_object_MSVC_name(f), inner(std::move(it)) {
 		}
 
-		OEL_ALWAYS_INLINE const Empty_function_object_MSVC_name & Func() const noexcept { return *this; }
+		OEL_ALWAYS_INLINE const Empty_function_object_MSVC_name & Func() const { return *this; }
+	};
+
+
+	template< typename Iter, typename Func,
+	          bool = std::is_move_assignable<Func>::value >
+	struct TransformIterHelp
+	{
+		TightPair<Iter, Func> m;
+
+		TransformIterHelp(Func f, Iter it)
+		 :	m{std::move(it), std::move(f)} {
+		}
+	};
+
+	template< typename Iter, typename Func >
+	struct TransformIterHelp<Iter, Func, false>
+	{
+		TightPair<Iter, Func> m;
+
+		TransformIterHelp(Func f, Iter it)
+		 :	m{std::move(it), std::move(f)} {
+		}
+
+		TransformIterHelp(TransformIterHelp && other)
+		 :	m{std::move(other.m.inner), other.m.Func()} {
+		}
+		TransformIterHelp(const TransformIterHelp &) = default;
+
+		TransformIterHelp & operator =(TransformIterHelp && other)
+		{
+			m.inner = std::move(other.m.inner);
+			return *this;
+		}
+		TransformIterHelp & operator =(const TransformIterHelp & other)
+		{
+			m.inner = other.m.inner;
+			return *this;
+		}
 	};
 }
 
 
 /** @brief Similar to boost::transform_iterator
 *
-* Note that the transform function is kept for the whole lifetime. It can only be set by
-* constructor, and is untouched on assignment. The reason is zero-overhead lambda support  */
+* Note that the transform function to use must be set at construction,
+* assignment may leave it untouched. The reason is zero-overhead lambda support  */
 template< typename UnaryFunc, typename Iterator >
-class transform_iterator
+class transform_iterator  : private _detail::TransformIterHelp<Iterator, UnaryFunc>
 {
-	_detail::TightPair<Iterator, UnaryFunc> _m;
+	using _impl = _detail::TransformIterHelp<Iterator, UnaryFunc>;
 
 public:
 	using iterator_category = typename std::conditional<
-			std::is_base_of< std::forward_iterator_tag, iter_category<Iterator> >::value,
+			std::is_base_of< std::forward_iterator_tag, iter_category<Iterator> >::value
+				and std::is_copy_constructible<_impl>::value,
 			std::forward_iterator_tag,
-			iter_category<Iterator>
+			std::input_iterator_tag
 		>::type;
 
 	using difference_type = iter_difference_t<Iterator>;
-	using reference       = decltype( _m.Func()(*_m.inner) );
+	using reference       = decltype( _impl::m.Func()(*_impl::m.inner) );
 	using pointer         = void;
 	using value_type      = typename std::decay<reference>::type;
 
-	Iterator base() const  { return _m.inner; }
+	using _impl::_impl;
 
-	transform_iterator(UnaryFunc f, Iterator it)
-	 :	_m{it, f} {
-	}
-
-	transform_iterator(const transform_iterator &) = default;
-
-	transform_iterator & operator =(const transform_iterator & other) &
-	{
-		_m.inner = other._m.inner;
-		return *this;
-	}
+	Iterator base() const &  { return this->m.inner; }
+	Iterator base() &&       { return std::move(this->m.inner); }
 
 	reference operator*() const
 	{
-		return _m.Func()(*_m.inner);
+		return this->m.Func()(*this->m.inner);
 	}
 
 	transform_iterator & operator++()  OEL_ALWAYS_INLINE
 	{	// preincrement
-		++_m.inner;
+		++this->m.inner;
 		return *this;
 	}
 
 	transform_iterator operator++(int) &
 	{	// postincrement
 		auto tmp = *this;
-		++_m.inner;
+		++this->m.inner;
 		return tmp;
 	}
 
 	template< typename Sentinel >  OEL_ALWAYS_INLINE
-	bool operator==(Sentinel right) const  { return _m.inner == right; }
+	bool operator==(Sentinel right) const  { return this->m.inner == right; }
 	template< typename Sentinel >  OEL_ALWAYS_INLINE
-	bool operator!=(Sentinel right) const  { return _m.inner != right; }
+	bool operator!=(Sentinel right) const  { return this->m.inner != right; }
 };
 
 } // namespace oel
