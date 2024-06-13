0c8fbf1fd3c395422d909a0405e8b5974fae5f5c
Ole Erik Peistorpet <OleErikPeistorpet@users.noreply.github.com>
2024-06-13 18:59:56 +0200

    as_(un)signed OEL_ALWAYS_INLINE again

--- util.h
+++ util.h
@@ -42,10 +42,12 @@ namespace oel
 
 //! Passed val of integral or enumeration type, returns val cast to the corresponding signed integer type
 inline constexpr auto as_signed =
-	[](auto val) noexcept -> std::make_signed_t<decltype(val)>    { return std::make_signed_t<decltype(val)>(val); };
+	[](auto val) noexcept -> std::make_signed_t<decltype(val)>
+		OEL_ALWAYS_INLINE { return std::make_signed_t<decltype(val)>(val); };
 //! Passed val of integral or enumeration type, returns val cast to the corresponding unsigned integer type
 inline constexpr auto as_unsigned =
-	[](auto val) noexcept -> std::make_unsigned_t<decltype(val)>  { return std::make_unsigned_t<decltype(val)>(val); };
+	[](auto val) noexcept -> std::make_unsigned_t<decltype(val)>
+		OEL_ALWAYS_INLINE { return std::make_unsigned_t<decltype(val)>(val); };
 
 
 /** @brief More generic than std::ssize, close to std::ranges::ssize
