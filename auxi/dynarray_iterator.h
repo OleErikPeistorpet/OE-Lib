b4cfc4766eab44910b9201eb633f6b9040b78dd1
Ole Erik Peistorpet <OleErikPeistorpet@users.noreply.github.com>
2025-04-23 15:29:40 +0200

    _iteratorFacade, dropped support for VS 2017

--- auxi/dynarray_iterator.h
+++ auxi/dynarray_iterator.h
@@ -7,7 +7,7 @@
 
 
 #include "dynarray_detail.h"
-#include "range_traits.h"
+#include "iterator_facade.h"
 
 
 #ifdef _MSC_VER
@@ -24,23 +24,14 @@ namespace oel
 inline namespace debug
 {
 
-/** @brief Checked iterator, for container with contiguous, dynamically allocated memory
-*
-* Note: a pair of value-initialized iterators count as an empty range (C++14 requirement)  */
 template< typename Ptr >
-class dynarray_iterator
+struct dynarray_iterator
+ :	public _iteratorFacade< dynarray_iterator<Ptr>, ptrdiff_t >
 {
 #define OEL_ITER_VALIDATE_DEREF  \
 	OEL_ASSERT( _header->id == _allocationId and _detail::HasValidIndex(_pElem, *_header) )
 
-#if OEL_MEM_BOUND_DEBUG_LVL >= 2
-	// Test for iterator pair pointing to same container
-	#define OEL_ITER_CHECK_COMPATIBLE(a, b)  OEL_ASSERT((a)._allocationId == (b)._allocationId)
-#else
-	#define OEL_ITER_CHECK_COMPATIBLE(a, b)
-#endif
 
-public:
 	using iterator_category = std::random_access_iterator_tag;
 #if __cpp_lib_concepts
 	using iterator_concept  = std::contiguous_iterator_tag;
@@ -54,128 +45,68 @@ public:
 	using const_iterator = dynarray_iterator<const value_type *>;
 
 	operator const_iterator() const noexcept  OEL_ALWAYS_INLINE
-	{
-		return {_pElem, _header, _allocationId};
-	}
+		{
+			return {{}, _pElem, _header, _allocationId};
+		}
 
 	reference operator*() const
-	{
-		OEL_ITER_VALIDATE_DEREF;
-		return *_pElem;
-	}
+		{
+			OEL_ITER_VALIDATE_DEREF;
+			return *_pElem;
+		}
 
 	pointer operator->() const
-	{
-		OEL_ITER_VALIDATE_DEREF;
-		return _pElem;
-	}
+		{
+			OEL_ITER_VALIDATE_DEREF;
+			return _pElem;
+		}
 
 	dynarray_iterator & operator++() &  OEL_ALWAYS_INLINE
-	{
-		++_pElem;
-		return *this;
-	}
+		{
+			++_pElem;
+			return *this;
+		}
 
 	dynarray_iterator operator++(int) &
-	{	// post-increment
-		auto tmp = *this;
-		++_pElem;
-		return tmp;
-	}
+		{	// post-increment
+			auto tmp = *this;
+			++_pElem;
+			return tmp;
+		}
 
 	dynarray_iterator & operator--() &  OEL_ALWAYS_INLINE
-	{
-		--_pElem;
-		return *this;
-	}
+		{
+			--_pElem;
+			return *this;
+		}
 
 	dynarray_iterator operator--(int) &
-	{	// post-decrement
-		auto tmp = *this;
-		--_pElem;
-		return tmp;
-	}
+		{	// post-decrement
+			auto tmp = *this;
+			--_pElem;
+			return tmp;
+		}
 
-	dynarray_iterator & operator+=(difference_type offset) &  OEL_ALWAYS_INLINE
-	{
-		_pElem += offset;
-		return *this;
-	}
-
-	dynarray_iterator & operator-=(difference_type offset) &  OEL_ALWAYS_INLINE
-	{
-		_pElem -= offset;
-		return *this;
-	}
-
-	friend dynarray_iterator operator +(difference_type offset, dynarray_iterator it)
-	{
-		it._pElem += offset;
-		return it;
-	}
-
-	[[nodiscard]] friend dynarray_iterator operator +(dynarray_iterator it, difference_type offset)
-	{
-		it._pElem += offset;
-		return it;
-	}
-
-	[[nodiscard]] friend dynarray_iterator operator -(dynarray_iterator it, difference_type offset)
-	{
-		it._pElem -= offset;
-		return it;
-	}
+	dynarray_iterator & operator+=(difference_type offset) &
+		{
+			_pElem += offset;
+			return *this;
+		}
 
 	friend difference_type operator -(const dynarray_iterator & left, const dynarray_iterator & right)
-	{
-		OEL_ITER_CHECK_COMPATIBLE(left, right);
-		return left._pElem - right._pElem;
-	}
+		{
+			return left._pElem - right._pElem;
+		}
 
-	reference operator[](difference_type offset) const
-	{
-		return *(*this + offset);
-	}
+	friend bool operator!=(const dynarray_iterator & left, const dynarray_iterator & right)
+		{
+			return left._pElem != right._pElem;
+		}
 
-	template< typename Ptr1 >
-	bool operator==(const dynarray_iterator<Ptr1> & right) const
-	{
-		OEL_ITER_CHECK_COMPATIBLE(*this, right);
-		return _pElem == right._pElem;
-	}
-
-	template< typename Ptr1 >
-	bool operator!=(const dynarray_iterator<Ptr1> & right) const
-	{
-		OEL_ITER_CHECK_COMPATIBLE(*this, right);
-		return _pElem != right._pElem;
-	}
-
-	template< typename Ptr1 >
-	bool operator <(const dynarray_iterator<Ptr1> & right) const
-	{
-		OEL_ITER_CHECK_COMPATIBLE(*this, right);
-		return _pElem < right._pElem;
-	}
-
-	template< typename Ptr1 >
-	bool operator >(const dynarray_iterator<Ptr1> & right) const
-	{
-		OEL_ITER_CHECK_COMPATIBLE(*this, right);
-		return _pElem > right._pElem;
-	}
-
-	template< typename Ptr1 >
-	bool operator<=(const dynarray_iterator<Ptr1> & right) const
-	{
-		return !(right < *this);
-	}
-
-	template< typename Ptr1 >
-	bool operator>=(const dynarray_iterator<Ptr1> & right) const
-	{
-		return !(*this < right);
-	}
+	friend bool operator <(const dynarray_iterator & left, const dynarray_iterator & right)
+		{
+			return left._pElem < right._pElem;
+		}
 
 
 	pointer _pElem; //!< Wrapped pointer. Treat the member variables as private!
@@ -183,7 +114,6 @@ public:
 	const _detail::DebugAllocationHeader * _header;
 	std::uintptr_t _allocationId;  //!< Used to check if this iterator has been invalidated by deallocation
 
-#undef OEL_ITER_CHECK_COMPATIBLE
 #undef OEL_ITER_VALIDATE_DEREF
 };
 
@@ -220,11 +150,11 @@ namespace oel::_detail
 		if (parent.data)
 		{
 			auto const h = _detail::DebugHeaderOf(parent.data);
-			return dynarray_iterator<Ptr>{pos, h, h->id};
+			return dynarray_iterator<Ptr>{{}, pos, h, h->id};
 		}
 		else
 		{	auto id = reinterpret_cast<std::uintptr_t>(&parent);
-			return dynarray_iterator<Ptr>{pos, &_detail::headerNoAllocation, id};
+			return dynarray_iterator<Ptr>{{}, pos, &_detail::headerNoAllocation, id};
 		}
 	#else
 		(void) parent;
