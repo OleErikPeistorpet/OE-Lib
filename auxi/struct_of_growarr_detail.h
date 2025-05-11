f2597aba70191fcb35e936edac545be31fd3b6e7
Ole Erik Peistorpet <OleErikPeistorpet@users.noreply.github.com>
2025-05-11 13:25:45 +0200

    struct_of_growarr: making usable for algorithms, and other WiP

--- auxi/struct_of_growarr_detail.h
+++ auxi/struct_of_growarr_detail.h
@@ -7,17 +7,84 @@
 
 
 #include "../view/counted.h"
+#include "../view/move.h"
 #include "../view/zip_transform.h"
+
+#include <tuple>
 
 
 namespace oel
 {
+namespace _detail
+{
+	constexpr auto to_tuple = [](auto &&... values)
+		{
+			return std::tuple{ static_cast< decltype(values) >(values)... };
+		};
+}
+
 
 using std::align_val_t;
 
 
+//! TODO doc
 template< typename, typename T, align_val_t = align_val_t{} >
 struct field_array  {};
+
+
+/*
+template< typename ElemStruct, typename ElemTag >
+constexpr auto convert_field_arrays =
+	[]< typename X, typename T, typename A >(field_array<X, T, A> &... fields)
+	{
+		return ElemStruct{ field_array {fields()}... };
+	};
+	*/
+template< typename ElemStruct >
+constexpr auto convert_field_arrays =
+	[](auto &... fields)
+	{
+		return ElemStruct{ {fields._val}... };
+	};
+
+template< typename ElemStructL, typename ElemStructR >
+auto assign_field_arrays(ElemStructL & left, const ElemStructR & right)
+	{
+		left._apply(_detail::to_tuple) = right._apply(_detail::to_tuple);
+	}
+
+template< typename ElemStruct >
+auto swap_field_arrays(ElemStruct & a, ElemStruct & b)
+	{
+		a._apply(_detail::to_tuple).swap( b._apply(_detail::to_tuple) );
+	}
+
+
+//! TODO doc
+#define OEL_STRUCT_OF_GROWARR_FIELDS(ElemStruct, ...)  \
+	\
+	template< typename F >       \
+	auto _apply(F fn_) const     \
+	{                            \
+		return fn_(__VA_ARGS__); \
+	}                            \
+	\
+	template< typename X >                                         \
+	operator ElemStruct<X>()                                       \
+	{                                                              \
+		return _apply(oel::convert_field_arrays< ElemStruct<X> >); \
+	}                                                              \
+	\
+	template< typename X >                         \
+	void operator =(const ElemStruct<X> & other_)  \
+	{                                              \
+		oel::assign_field_arrays(*this, other_);   \
+	}                                              \
+	\
+	friend void swap(ElemStruct & struct0, ElemStruct & struct1)  \
+	{                                                             \
+		oel::swap_field_arrays(struct0, struct1);                 \
+	}
 
 
 
@@ -79,8 +146,20 @@ namespace _detail
 	};
 
 
+	template
+	<	typename Ptr,
+		typename Base = decltype( view::counted<Ptr>() | view::move )
+	>
+	struct CountedMoveView : public Base
+	{
+		CountedMoveView(Ptr p, ptrdiff_t n)
+		 :	Base{ {}, {p, n} } {}
+	};
+
+
 	struct ViewTag {};
 	struct ConstViewTag {};
+	struct RvalueViewTag {};
 	struct ElementTag {};
 	struct ConstElementTag {};
 	struct RvalueElementTag {};
@@ -108,6 +187,11 @@ struct field_array<_detail::ConstViewTag, T, A>
  :	public view::counted<const T *>
 {};
 
+template< typename T, align_val_t A >
+struct field_array<_detail::RvalueViewTag, T, A>
+ :	public _detail::CountedMoveView<T *>
+{};
+
 
 template< typename T, align_val_t A >
 struct field_array<_detail::ElementTag, T, A>
@@ -116,6 +200,19 @@ struct field_array<_detail::ElementTag, T, A>
 
 	T &       operator()() noexcept        { return _val; }
 	const T & operator()() const noexcept  { return _val; }
+
+	template< typename ElemTag >
+	void operator =(field_array<ElemTag, T, A> other)
+		{
+			_val = other();
+		}
+
+	friend void swap(field_array & a, field_array & b)
+		noexcept(std::is_nothrow_swappable<T>)
+		{
+			using std::swap;
+			swap(a._val, b._val);
+		}
 };
 
 template< typename T, align_val_t A >
