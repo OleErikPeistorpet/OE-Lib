7c5e97048efce742c71920936ba4dc4e25237a3e
Ole Erik Peistorpet <OleErikPeistorpet@users.noreply.github.com>
2026-04-27 00:13:37 +0200

    Ditched reverse_iterator type member of dynarray

--- dynarray.h
+++ dynarray.h
@@ -77,9 +77,6 @@ public:
 	using iterator       = T *;
 	using const_iterator = const T *;
 #endif
-	using reverse_iterator       = std::reverse_iterator<iterator>;
-	using const_reverse_iterator = std::reverse_iterator<const_iterator>;
-
 
 	constexpr dynarray() noexcept(noexcept( Alloc{} ))  : dynarray(Alloc{}) {}
 	constexpr explicit dynarray(Alloc a) noexcept       : _m(a) {}
@@ -222,13 +219,13 @@ public:
 	OEL_ALWAYS_INLINE
 	const_iterator cend() const noexcept   { return end(); }
 
-	reverse_iterator       rbegin() noexcept          { return       reverse_iterator{end()}; }
-	const_reverse_iterator rbegin() const noexcept    { return const_reverse_iterator{end()}; }
-	const_reverse_iterator crbegin() const noexcept   { return const_reverse_iterator{end()}; }
+	auto      rbegin() noexcept         { return std::reverse_iterator{end()}; }
+	auto      rbegin() const noexcept   { return std::reverse_iterator{end()}; }
+	auto      crbegin() const noexcept  { return std::reverse_iterator{end()}; }
 
-	reverse_iterator       rend() noexcept          { return       reverse_iterator{begin()}; }
-	const_reverse_iterator rend() const noexcept    { return const_reverse_iterator{begin()}; }
-	const_reverse_iterator crend() const noexcept   { return const_reverse_iterator{begin()}; }
+	auto      rend() noexcept         { return std::reverse_iterator{begin()}; }
+	auto      rend() const noexcept   { return std::reverse_iterator{begin()}; }
+	auto      crend() const noexcept  { return std::reverse_iterator{begin()}; }
 
 	T *       data() noexcept         { return _m.data; }
 	const T * data() const noexcept   { return _m.data; }
